ca5ed4509d38f3ea74cf65c1d3515c77758370fa
2YoungHoon <djaakclsrn12@naver.com>
2022-05-16 22:45:10 +0900

    소수구하기

---
+++ 소수구하기.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        // 에라토스테네스의 체
+        int[] filter = new int[n + 1];
+        Arrays.fill(filter, 1);
+        // 0, 1은 소수가 아님
+        filter[0] = 0;
+        filter[1] = 0;
+
+        for (int i = 2; i < Math.sqrt(n) + 1; i++){
+
+            if (filter[i] == 1){
+                for (int j = i * 2; j <= n; j+=i){
+                    filter[j] = 0;
+                }
+            }
+        }
+
+        for (int check: filter){
+            if (check == 1){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
