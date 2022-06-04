36aecf4800d71c04535509f08075cf052e046731
2YoungHoon <djaakclsrn12@naver.com>
2022-06-04 18:01:16 +0900

    행렬의덧셈

---
+++ 행렬의덧셈.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int size = arr1.length;
+        int size1 = arr1[0].length;
+        int[][] answer = new int[size][size1];
+          
+        for ( int i = 0; i < size; i++ ){
+            for ( int j = 0; j < size1; j++ ){
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
